f1ffc8852c4e91c7b8cdd65d838e5a8d7b7456a2
Анна Федорова <annafedorova@MacBook-Air-Anna-6.local>
2025-11-14 13:03:39 +0300

    task 8 in progress

---
+++ 08_I18n/src/guess.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <libintl.h>
+#include <locale.h>
+
+#define _(STRING) gettext(STRING)
+
+int main(int argc, char **argv) {
+	setlocale(LC_ALL, "");
+	bindtextdomain("guess_number", ".");
+	textdomain("guess_number");
+
+    printf(_("Think of a number from 1 to 100: \n"));
+
+    char responce[256];
+    int start = 1, end = 100, mid;
+
+    while (start < end) {
+        mid = (start + end) / 2;
+        printf(_("Is your number bigger than %d? (%s/%s): "), mid, _("yes"), _("no"));
+        fflush(stdout);
+
+        if (scanf("%255s", response) != 1) {
+            fprintf(stderr, _("Read error\n"));
+            return 1;
+        }
+
+        if (strcmp(responce, _("yes")) == 0) {
+            start = mid + 1;
+        } else if (strcmp(responce, _("no")) == 0) {
+            end = mid;
+        } else {
+            fprintf(stderr, _("Invalid input error. Enter '%s' or '%s'.\n"), _("yes"), _("no"));
+            continue;
+        }
+    }
+
+    printf(_("Your number is %d\n"), start);
+    return 0;
+}
